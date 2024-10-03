2f3ec54d328a216fc9a06bedb436dbc5e58529e7
cam <cameronayres123@gmail.com>
2024-10-03 17:50:19 +0100

    Added equality checking functions.

--- lisp-on-arduino.ino
+++ lisp-on-arduino.ino
@@ -51,3 +51,19 @@ struct cons append(struct cons *list, int data) {
 
 	return(list);
 }
+
+int atom_equal(struct atom *atom_one, struct atom *atom_two) {
+	if(atom_one->data == atom_two->data) return(0);
+	return(-1);
+}
+
+int list_equal(struct cons *list_one, struct cons *list_two) {
+	struct cons cdr_left = list_one;
+	struct cons cdr_right = list_two
+	while(cdr_left->next != 0x0 && cdr_right->next != 0x0) {
+		if(cdr_left->car->data != cdr_right->car->data) return(-1);
+
+		cdr_left = cdr_left->cdr;
+		cdr_right = cdr_right->cdr;
+	}
+}

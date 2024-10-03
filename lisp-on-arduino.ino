dff9eff5f4d966973c4a64f24b355a43e47d2096
cam <cameronayres123@gmail.com>
2024-10-03 17:19:01 +0100

    Added an atom type.

---
+++ lisp-on-arduino.ino
@@ -0,0 +1,27 @@
+struct atom {
+	void *data;
+}
+
+int atom_init(struct atom *atom) {
+	atom = malloc(sizeof(struct atom));
+}
+
+int atom_write(int data, struct atom *atom) {
+	atom->data = malloc(sizeof(int));
+	atom->*data = data;
+
+	return(0);
+}
+
+int atom_read(struct atom *atom) {
+	if(atom == NULL) return(-1);
+	
+	Serial.println(atom->data);
+	return(0);
+}
+
+int atom_gc(struct atom *atom) {
+	if(atom == 0x0) return(-1);
+	free(atom);
+	return(0);
+}
